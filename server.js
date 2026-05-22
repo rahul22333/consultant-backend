ed62984528736e9d3fc236a34bf0e0e8013302b3
rahul <animenew122333@gmail.com>
2026-05-22 19:47:40 +0530

    rate limiter added

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ import cors from "cors";
 import paymentRoutes from "./routes/payment.js";
 import slotRoutes from "./routes/slotRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import helmet from "helmet";
+
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -27,18 +31,53 @@ app.use(
 );
 app.use(express.json());
 
+app.use(helmet());
+
 // ✅ Routes
-app.use("/api/payment", paymentRoutes);
 app.use("/api/slots", slotRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
 
 // ✅ Health check
 app.get("/", (req, res) => {
   res.send("Backend running 🚀");
 });
 
-// ❌ REMOVE THIS (was leaking env)
-// console.log("ENV CHECK:", process.env);
+// rate limit
+const limiter = rateLimit({
+
+  windowMs: 15 * 60 * 1000,
+
+  max: 100,
+
+  message: {
+    success: false,
+    message:
+      "Too many requests, try again later.",
+  },
+});
+
+app.use(limiter);
+
+//Strict Payment Limiter
+const paymentLimiter = rateLimit({
+
+  windowMs: 5 * 60 * 1000,
+
+  max: 10,
+
+  message: {
+    success: false,
+    message:
+      "Too many payment attempts",
+  },
+});
+
+app.use(
+  "/api/payment",
+  paymentLimiter,
+  paymentRoutes
+);
 
 const PORT = process.env.PORT || 5000;
 
